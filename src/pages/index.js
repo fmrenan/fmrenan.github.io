3b2abb4dc3f8cdbad8c95b5f7ba92411fd3f47e6
Renan Muniz <fmrenan@outlook.com>
2021-04-28 22:01:48 -0300

    Created nav and card

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,28 @@
-import Head from 'next/head'
+
+import { Card } from '../components/Card'
 
 import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <div className={styles.homePage}>
-      <Head>
-        <title>Renan Muniz</title>
-      </Head>
-    </div>
+    <main className={styles.main}>
+      <nav>
+        <ul>
+          <li>
+            <a href="#" className={styles.active}>about</a>
+          </li>
+          
+          <li>
+            <a href="#" >projects</a>
+          </li>
+          
+          <li>
+            <a href="#" >contact</a>
+          </li>
+        </ul>
+      </nav>
+      
+      <Card />
+    </main>
   )
 }
